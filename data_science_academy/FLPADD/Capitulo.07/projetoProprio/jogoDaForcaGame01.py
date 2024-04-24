ebb89e83f90589afb5cb150b5db7ca0d193f5b53
William campos <39564940+soueuwilliam@users.noreply.github.com>
2024-04-24 20:01:32 +0000

    adicionado arquivos e começando projeto do jogo da forca

---
+++ data_science_academy/FLPADD/Capitulo.07/projetoProprio/jogoDaForcaGame01.py
@@ -0,0 +1,14 @@
+from random import choice
+
+palavras = ["maçã", "banana", "laranja", "uva", "morango", "abacaxi", "melancia", "mamão", "pera", "kiwi"]
+
+chances = 6
+palavra_sorteada = choice(palavras)
+
+
+while chances > 6:
+  
+
+
+
+
