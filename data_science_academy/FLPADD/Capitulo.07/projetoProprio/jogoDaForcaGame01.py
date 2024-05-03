23018ff09ea4cc883452f2f49b7af9a7c8dd37fb
william campos <camposwilliam23@gmail.com>
2024-05-03 17:14:56 -0300

    adicioando arquivos do capitulo 08 e projeto 01

--- data_science_academy/FLPADD/Capitulo.07/projetoProprio/jogoDaForcaGame01.py
+++ data_science_academy/FLPADD/Capitulo.07/projetoProprio/jogoDaForcaGame01.py
@@ -1,14 +1,56 @@
 from random import choice
+from os import system,name
 
-palavras = ["maçã", "banana", "laranja", "uva", "morango", "abacaxi", "melancia", "mamão", "pera", "kiwi"]
-
-chances = 6
+#Lista de palavras jogo
+palavras = ["maca", "banana", "laranja", "uva", "morango", "abacaxi", "melancia", "mamao", "pera", "kiwi"]
 palavra_sorteada = choice(palavras)
+letrasAdivinhadas = list()
+letrasErradas= list()
+chances = 6
 
+def limparTela():
+    if name == 'nt':
+        _ = system("cls")
+    else:
+        _ =system("clear")
 
-while chances > 6:
-  
+def mostrarLetrasErradas(*letrasErradas):
+    if len(letrasErradas) == 0 :
+        return ''
+    print(f'\nLetras Erradas: ',end=' ')
+    for e,c in enumerate(letrasErradas):
+        print(f'{c}', end= ' ')
 
-
-
-
+def palavraSorteada(palavraSorteada,letrasAdivinhadas):
+    palavraDescoberta = ['-' for letra in palavraSorteada]
+    for e,c in enumerate(palavraSorteada):
+        if c in letrasAdivinhadas:
+            print(f'{c}',end=" ")
+            palavraDescoberta[e] = c         
+        else:
+            print('_',end=" ")
+    if '-' not in palavraDescoberta:
+        return True
+    
+while True:
+    limparTela()
+    print(f"Bem vindo(a) ao jogo da forca!")
+    print("Adivinhe a palavra abaixo")
+    if chances > 0:
+        ganhou = palavraSorteada(palavra_sorteada,letrasAdivinhadas)
+        print(f'\nNumeros de chances : {chances}')
+        mostrarLetrasErradas(letrasErradas)
+        letra = str(input("\nDigite a letra: "))[0]
+        if letra in palavra_sorteada:
+            letrasAdivinhadas.append(letra)
+        else:
+            if letra not in letrasErradas:
+                letrasErradas.append(letra)
+            chances-=1
+    else:
+        print(f'Você perdeu a palavra era {palavra_sorteada}') 
+        break
+    if ganhou == True:
+        print("Parabéns você ganhou o jogo e advinhou a palavra !!!!!!!!!")
+        break
+       
